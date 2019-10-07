e52adb4069b06269fa8f7472f87334f780644638
maricela <laram@mail.gvsu.edu>
2019-10-07 18:31:44 -0400

    Lab 21 commit

---
+++ Models/Users.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab21.Models
+{
+    public class Users
+    {
+        [Key]
+        [Required(ErrorMessage = "First Name is required")]
+        public string FirstName { get; set; }
+
+
+        [Required(ErrorMessage = "Last Name is required")]
+        public string LastName { get; set; }
+
+
+        [Required(ErrorMessage = "Email is required")]
+        public string Email { get; set; }
+
+
+        [Required(ErrorMessage = "Phone Number is required")]
+        public int PhoneNumber { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        [StringLength(255, ErrorMessage = "Must be between 5 and 255 characters", MinimumLength = 5)]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
